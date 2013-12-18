713b3323ef5fc825a88c54542e6fc30634448108
Ben Fulcher <ben.fulcher@gmail.com>
2013-12-18 17:35:48 +1100

    You can now specify ‘all’,’nocalctime’,’outputs’, or ‘quality’ in TSQ_prepared. This allows you to retrieve smaller amounts of information for quicker retrieval from the database, as required

--- Calculation/TSQ_prepared.m
+++ Calculation/TSQ_prepared.m
@@ -5,16 +5,32 @@
 % operations to include, and outputs the relevant subsection of the data matrix
 % and associated metadata in HCTSA_loc
 % 
-% ---OUTPUT
-% (*) DidWrite [opt] is 1 if Writes new file HCTSA_loc.mat
+%---INPUTS:
+%--ts_ids: a vector of ts_ids to retrieve from the mySQL database.
+%--op_ids: a vector of op_ids to retrieve from the mySQL database.
+%--RetrieveWhatEntries: can be one of the following options:
+%        (i) 'null': Retrieve null entries from the database (e.g., for
+%                    calculating)
+%        (ii) 'all': Retrieve all entries from the database (e.g., for
+%                    analyzing)
+%        (iii) 'error': Retrieve previous errors stored in the database (e.g.,
+%                       for re-evaluating previous errors)
+%--RetrieveWhatData: can be one of the following options:
+%       (i) 'all': Retrieves Data, CalcTimes, and Quality labels
+%       (ii) 'nocalctime': Retrieves calculated values and quality labels
+%       (iii) 'outputs': Just the calculated values
+%       (iv) 'quality': Just the quality labels
 % 
-% --- Other outputs are to the file HCTSA_loc.mat contains
-% (*) TS_DataMat, contains the data
-% (*) TS_CalcTime, contains the calculation times
-% (*) TS_Quality, contains the quality codes
-% (*) TimeSeries, contains metadata about the time series, and the time-series data
-% (*) Operations, contains metadata about the operations
-% (*) MasterOperations, contains metadata about the implicatedmaster operations
+%---OUTPUT:
+%--DidWrite [opt] is 1 if Writes new file HCTSA_loc.mat
+% 
+% Other outputs are to the file HCTSA_loc.mat contains
+%--TS_DataMat, contains the data
+%--TS_Quality, contains the quality codes
+%--TS_CalcTime, contains the calculation times [if RetrieveWhatData = 'all']
+%--TimeSeries, contains metadata about the time series, and the time-series data
+%--Operations, contains metadata about the operations
+%--MasterOperations, contains metadata about the implicatedmaster operations
 % 
 % ------------------------------------------------------------------------------
 % Copyright (C) 2013,  Ben D. Fulcher <ben.d.fulcher@gmail.com>,
@@ -32,35 +48,42 @@
 % California, 94041, USA.
 % ------------------------------------------------------------------------------
 
-function DidWrite = TSQ_prepared(ts_ids, op_ids, RetrieveWhat, brawninputs, doinone)
+function DidWrite = TSQ_prepared(ts_ids, op_ids, RetrieveWhatEntries, RetrieveWhatData)
     
 % Until it actually writes, set DidWrite = 0
 DidWrite = 0;
 
-%% Check inputs -- set defaults
+%% Check inputs and set defaults
 if nargin < 2
 	error('You must provide at least two inputs!');
 end
-if nargin < 3
-	RetrieveWhat = 'all'; % retrieve full sets of things, not just the empty entries in the database
-    fprintf(1,'Retrieving ALL elements from the database in the specified ranges by default.\n')
+
+% RetrieveWhatEntries
+if nargin < 3 || isempty(RetrieveWhatEntries)
+	RetrieveWhatEntries = 'all'; % retrieve full sets of things, not just the empty entries in the database
+    fprintf(1,'Retrieving ALL elements from the database in the specified ts_id and op_id ranges by default.\n')
 end
-RetrieveWhatcanbe = {'null','all','error'};
-if ~ischar(RetrieveWhat) || ~ismember(RetrieveWhat,RetrieveWhatcanbe)
-    error('The third input to TSQ_prepared must specify what to retrieve, one of the following: %s',BF_cat(RetrieveWhatcanbe))
-end
-if nargin < 4 || isempty(brawninputs)
-	brawninputs = [1, 1]; % log and parallelize -- only relevant if RetrieveWhat isn't empty
-end
-if nargin < 5 || isempty(doinone)
-	doinone = 0; % make seperate connections so as not to overwhelm java heap space
+RetrieveWhatEntriesCanBe = {'null','all','error'};
+if ~ischar(RetrieveWhatEntries) || ~ismember(RetrieveWhatEntries,RetrieveWhatEntriesCanBe)
+    error(['The third input to TSQ_prepared must specify what type of entries to retrieve from ' ...
+                'the database, one of the following: %s'],BF_cat(RetrieveWhatEntriesCanBe))
 end
 
-%% METHOD 1: Get entries of results table for local Matlab matrix (1 row/column at a time)
-% we have ts_ids and op_ids
-% To put in a matrix with rows (time series) and columns (metrics)
+% RetrieveWhatData
+if nargin < 4 || isempty(RetrieveWhatData)
+    RetrieveWhatData = 'nocalctime'; % Just get data and quality labels
+end
+RetrieveWhatDataCanBe = {'all','nocalctime','outputs','quality'};
+if ~ischar(RetrieveWhatData) || ~ismember(RetrieveWhatData,RetrieveWhatDataCanBe)
+    error(['The fourth input to TSQ_prepared must specify what data to retrieve from the database, ' ...
+                'one of the following: %s'],BF_cat(RetrieveWhatEntriesCanBe))
+end
+
+%% Get going
+% Retrieve from the database over many iterations, retrieving data from a given
+% number of time series at each iteration
 % Could do one big query and then reform to a matrix, but I'll do it row-by-row
-% In fact this is faster for some reason than doing a big query (method 2)
+% This is faster than doing one big query
 
 % Make sure ts_ids and op_ids are column vectors
 if size(ts_ids,2) > size(ts_ids,1), ts_ids = ts_ids'; end
@@ -118,16 +141,26 @@ fprintf(1,'Filling and saving to local Matlab file HCTSA_loc.mat from the Result
 %% Intialize matrices
 
 % Start as Infs to distinguish unwritten entries after database pull
-TS_DataMat = ones(nts,nops)*Inf;  % Outputs
-TS_CalcTime = ones(nts,nops)*Inf; % Calculation times
-TS_Quality = ones(nts,nops)*Inf;  % Quality labels
+switch RetrieveWhatData
+case 'all'
+    TS_DataMat = ones(nts,nops)*Inf;  % Outputs
+    TS_Quality = ones(nts,nops)*Inf;  % Quality labels
+    TS_CalcTime = ones(nts,nops)*Inf; % Calculation times
+case 'nocalctime'
+    TS_DataMat = ones(nts,nops)*Inf;  % Outputs
+    TS_Quality = ones(nts,nops)*Inf;  % Quality labels
+case 'outputs'
+    TS_DataMat = ones(nts,nops)*Inf;  % Outputs
+case 'quality'
+    TS_Quality = ones(nts,nops)*Inf;  % Quality labels
+end
 
 % Set BundleSize for retrieving
-BundleSize = min(nts,5); % Retrieve information about this many time series per database query:
-                         % either 5 at a time, or if less, however many time series there are
+BundleSize = min(nts,1); % Retrieve data for this many time series per database query
+                         % either 2 at a time, or if less, however many time series there are
 
-%% Provide user information
-switch RetrieveWhat
+%% Display information
+switch RetrieveWhatEntries
 case 'all' 
     fprintf(1,['Retrieving all elements from the database (in groups of %u time series ' ...
                 'per database query). Please be patient...\n'],BundleSize);
@@ -139,20 +172,35 @@ case 'error'
                 'per database query). Please be patient...\n'],BundleSize);
 end
 
-Bundles = (1:BundleSize:length(ts_ids));
-NumIterations = length(Bundles); % Number of iterations of the loop
+Bundles = (1:BundleSize:length(ts_ids)); % Bundles of time series for each iteration
+NumIterations = length(Bundles);         % Number of iterations of the loop
 IterationTimes = zeros(NumIterations,1); % Record the time taken for each iteration
-DidRetrieve = zeros(NumIterations,1); % Keep track of whether data was retrieved at each iteration
+DidRetrieve = zeros(NumIterations,1);    % Keep track of whether data was retrieved at each iteration
 
 % First select the data:
 for i = 1:NumIterations
-    IterationTimer = tic; % Time this iteration using IterationTimer
+    
+    IterationTimer = tic; % Time each iteration using IterationTimer
 
-    ii = (Bundles(i):1:min(Bundles(i)+BundleSize-1,length(ts_ids))); % indicies for this iteration
-    ts_ids_now = ts_ids(ii); % a range of ts_ids to retrieve in this iteration
-    BaseString = sprintf(['SELECT ts_id, op_id, Output, CalculationTime, QualityCode FROM Results WHERE ' ...
-                    	'ts_id IN (%s) AND op_id IN (%s)'],BF_cat(ts_ids_now,','),op_ids_string);
-    switch RetrieveWhat
+    ii = (Bundles(i):1:min(Bundles(i)+BundleSize-1,length(ts_ids))); % Indicies for this iteration
+    ts_ids_now = ts_ids(ii); % Range of ts_ids retrieved in this iteration
+    
+    % Start piecing together the mySQL SELECT command:
+    switch RetrieveWhatData
+    case 'all'
+        SelectWhat = 'SELECT ts_id, op_id, Output, QualityCode, CalculationTime FROM Results';
+    case 'nocalctime' % Do not retrieve calculation time results
+        SelectWhat = 'SELECT ts_id, op_id, Output, QualityCode FROM Results';
+    case 'outputs'
+        SelectWhat = 'SELECT ts_id, op_id, Output FROM Results';
+    case 'quality'
+        SelectWhat = 'SELECT ts_id, op_id, QualityCode FROM Results';
+    end
+    
+    BaseString = sprintf('%s WHERE ts_id IN (%s) AND op_id IN (%s)',SelectWhat, ...
+                                            BF_cat(ts_ids_now,','),op_ids_string);
+    
+    switch RetrieveWhatEntries
     case 'all'
         SelectString = BaseString;
     case 'null'
@@ -161,63 +209,101 @@ for i = 1:NumIterations
     	SelectString = sprintf('%s AND QualityCode = 1',BaseString);
     end
     
+    % Do the retrieval
+    % DatabaseTimer = tic;
 	[qrc, ~, ~, emsg] = mysql_dbquery(dbc,SelectString); % Retrieve this bundle of data from the database
+    % fprintf(1,'Database query for %u time series took %s\n',BundleSize,BF_thetime(toc(DatabaseTimer)));
     
-    % Check results look ok:
     if ~isempty(emsg)
         error('Error retrieving outputs from %s!!! :(\n%s',dbname,emsg);
     end
     
+    % Check results look ok:
     if (size(qrc) == 0) % There are no entries in Results that match the requested conditions
         fprintf(1,'No data to retrieve for ts_id = %s\n',BF_cat(ts_ids_now,','));
-        % Leave local files TS_DataMat, TS_CalcTime, and TS_Quality as Inf
+        % Leave local files TS_DataMat, TS_Quality (and TS_CalcTime) as Inf
         
-    else % Entries need to be written to local matrices
+    else
+        % Entries need to be written to local matrices
         % Set DidRetrieve = 1 for this iteration
         DidRetrieve(i) = 1;
         
     	% Convert empty entries to NaNs
     	qrc(cellfun(@isempty,qrc)) = {NaN};
     
-        % Fill rows of local matrix
-        if strcmp(RetrieveWhat,'all') % easy in this case
-            TS_DataMat(ii,:) = reshape(vertcat(qrc{:,3}),length(ii),nops);
-            TS_CalcTime(ii,:) = reshape(vertcat(qrc{:,4}),length(ii),nops);
-            TS_Quality(ii,:) = reshape(vertcat(qrc{:,5}),length(ii),nops);
+        % Put results from database into rows of local matrix
+        if strcmp(RetrieveWhatEntries,'all') % easy in this case
+            switch RetrieveWhatData
+            case 'all'
+                TS_DataMat(ii,:) = reshape(vertcat(qrc{:,3}),length(ii),nops);
+                TS_Quality(ii,:) = reshape(vertcat(qrc{:,4}),length(ii),nops);
+                TS_CalcTime(ii,:) = reshape(vertcat(qrc{:,5}),length(ii),nops);
+            case 'nocalctime'
+                TS_DataMat(ii,:) = reshape(vertcat(qrc{:,3}),length(ii),nops);
+                TS_Quality(ii,:) = reshape(vertcat(qrc{:,4}),length(ii),nops);
+            case 'outputs'
+                TS_DataMat(ii,:) = reshape(vertcat(qrc{:,3}),length(ii),nops);
+            case 'quality'
+                TS_Quality(ii,:) = reshape(vertcat(qrc{:,4}),length(ii),nops);
+            end
         else
             % We need to match retrieved indicies to the local indicies
             ix = arrayfun(@(x)find(ts_ids_now == x,1),vertcat(qrc{:,1}));
             iy = arrayfun(@(x)find(op_ids == x,1),vertcat(qrc{:,2}));
-            for k = 1:length(ix) % fill it one entry at a time
-                TS_DataMat(ix(k),iy(k)) = qrc{k,3};
-                TS_CalcTime(ix(k),iy(k)) = qrc{k,4};
-                TS_Quality(ix(k),iy(k)) = qrc{k,5};
+            switch RetrieveWhatData
+            case 'all'
+                for k = 1:length(ix) % fill it one entry at a time
+                    TS_DataMat(ix(k),iy(k)) = qrc{k,3};
+                    TS_Quality(ix(k),iy(k)) = qrc{k,4};
+                    TS_CalcTime(ix(k),iy(k)) = qrc{k,5};
+                end
+            case 'nocalctime'
+                for k = 1:length(ix) % fill it one entry at a time
+                    TS_DataMat(ix(k),iy(k)) = qrc{k,3};
+                    TS_Quality(ix(k),iy(k)) = qrc{k,4};
+                end
+            case 'outputs'
+                for k = 1:length(ix) % fill it one entry at a time
+                    TS_DataMat(ix(k),iy(k)) = qrc{k,3};
+                end
+            case 'quality'
+                for k = 1:length(ix) % fill it one entry at a time
+                    TS_Quality(ix(k),iy(k)) = qrc{k,4};
+                end
             end
         end
     end
     
-    % Note time taken for this iteration
+    % Note time taken for this iteration, and periodically display indication of time remaining
 	IterationTimes(i) = toc(IterationTimer);
-	if mod(i,floor(NumIterations/10)) == 0 % Tell us the time remaining 10 times
-		fprintf(1,'Approximately %s remaining...\n',BF_thetime(mean(IterationTimes(1:i))*(nts-i)));
+    if (i==1) % Give an initial indication of time after the first iteration
+        fprintf(1,['Based on the first retrieval, this is taking ' ...
+                'approximately %s per time series...\n'],BF_thetime(IterationTimes(1)/BundleSize));
+		fprintf(1,'Approximately %s remaining...\n',BF_thetime(IterationTimes(1)*(NumIterations-1)));
+    elseif (mod(i,floor(NumIterations/10))==0) % Tell us the time remaining 10 times across the total retrieval
+		fprintf(1,'Approximately %s remaining...\n',BF_thetime(mean(IterationTimes(1:i))*(NumIterations-i)));
 	end
 end
+
+% Finished retrieving from the database!
 if any(DidRetrieve)
-    fprintf(1,'Retrieved data from %s over %u iteration(s) in %s.\n',dbname,NumIterations,BF_thetime(sum(IterationTimes)));
+    fprintf(1,'Retrieved data from %s in %u iterations in %s.\n',dbname,NumIterations,BF_thetime(sum(IterationTimes)));
 else
-    fprintf(1,'Over %u iteration(s), no data was retrieved from %s.\nNot writing any data to file.\n',NumIterations,dbname);
+    fprintf(1,'Over %u iterations, no data was retrieved from %s.\nNot writing any data to file.\n',NumIterations,dbname);
     SQL_closedatabase(dbc); return
 end
 	
-if ismember(RetrieveWhat,{'null','error'})    
-    % We only want to keep rows and columns with (e.g., NaNs) in them...
-    switch RetrieveWhat
+if ismember(RetrieveWhatEntries,{'null','error'})    
+    % We only want to keep rows and columns with (NaNs for 'null' or errors for 'error') in them...
+    switch RetrieveWhatEntries
     case 'null'
         keepme = isnan(TS_DataMat); % NULLs in database
-    	fprintf(1,'Filtering so that local files contain rows/columns containing at least one NULL entry\n');
+        fprintf(1,['Filtering so that local files contain rows/columns containing at least ' ...
+                                'one entry that was NULL in the database.\n']);
     case 'error'
         keepme = (TS_Quality == 1); % Error codes in database
-    	fprintf(1,'Filtering so that local files contain rows/columns containing at least one entry that was an error in the database\n');
+        fprintf(1,['Filtering so that local files contain rows/columns containing at least ' ...
+                                'one entry that was an error in the database.\n']);
     end
     
 	% Time series
@@ -229,7 +315,19 @@ if ismember(RetrieveWhat,{'null','error'})
 		fprintf(1,'Cutting down from %u to %u time series\n',nts,sum(keepi));
 		ts_ids = ts_ids(keepi); nts = length(ts_ids);
 		ts_ids_string = BF_cat(ts_ids,',');
-		TS_DataMat = TS_DataMat(keepi,:); TS_CalcTime = TS_CalcTime(keepi,:); TS_Quality = TS_Quality(keepi,:); % update local data stores
+        switch RetrieveWhatData
+        case 'all'
+    		TS_DataMat = TS_DataMat(keepi,:);
+            TS_Quality = TS_Quality(keepi,:);
+            TS_CalcTime = TS_CalcTime(keepi,:);
+        case 'nocalctime'
+    		TS_DataMat = TS_DataMat(keepi,:);
+            TS_Quality = TS_Quality(keepi,:);
+        case 'outputs'
+            TS_DataMat = TS_DataMat(keepi,:);
+        case 'quality'
+            TS_Quality = TS_Quality(keepi,:);
+        end
 	end
 	
 	% Operations
@@ -241,7 +339,19 @@ if ismember(RetrieveWhat,{'null','error'})
 		fprintf(1,'Cutting down from %u to %u operations\n',nops,sum(keepi));
 		op_ids = op_ids(keepi); nops = length(op_ids);
 		op_ids_string = BF_cat(op_ids,',');
-		TS_DataMat = TS_DataMat(:,keepi); TS_CalcTime = TS_CalcTime(:,keepi); TS_Quality = TS_Quality(:,keepi);
+        switch RetrieveWhatData
+        case 'all'
+    		TS_DataMat = TS_DataMat(:,keepi);
+            TS_Quality = TS_Quality(:,keepi);
+            TS_CalcTime = TS_CalcTime(:,keepi);
+        case 'nocalctime' 
+    		TS_DataMat = TS_DataMat(:,keepi);
+            TS_Quality = TS_Quality(:,keepi);
+        case 'outputs'
+            TS_DataMat = TS_DataMat(:,keepi);
+        case 'quality'
+            TS_Quality = TS_Quality(:,keepi);
+        end
 	end    
 end
 
@@ -280,7 +390,22 @@ SQL_closedatabase(dbc)
 
 % Save to HCTSA_loc.mat
 fprintf(1,'Saving local versions of the data...');
-save('HCTSA_loc.mat','TS_DataMat','TS_CalcTime','TS_Quality','TimeSeries','Operations','MasterOperations','-v7.3');
+save('HCTSA_loc.mat','TS_DataMat','TS_Quality','TimeSeries','Operations','MasterOperations','-v7.3');
+switch RetrieveWhatData
+case 'all'
+    % Add outputs, quality labels, and calculation times
+    save('HCTSA_loc.mat','TS_DataMat','TS_Quality','TS_CalcTime','-append')
+case 'nocalctime'
+    % Add outputs and quality labels
+    save('HCTSA_loc.mat','TS_DataMat','TS_Quality','-append')
+case 'outputs'
+    % Add outputs
+    save('HCTSA_loc.mat','TS_DataMat','-append')
+case 'quality'
+    % Add quality labels
+    save('HCTSA_loc.mat','TS_Quality','-append')
+end
+
 fprintf(1,' Done.\n');
 DidWrite = 1;
 
