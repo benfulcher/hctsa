58100cc604d0840379f08b238d77de4ad43a376b
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-09-27 18:14:40 +1000

    Aesthetic tweaks to TS_plot_2d

--- PlottingAnalysis/TS_plot_2d.m
+++ PlottingAnalysis/TS_plot_2d.m
@@ -1,10 +1,10 @@
-function TS_plot_2d(Features,TimeSeries,featureLabels,groupNames,annotateParams,showDistr,whatClassifier)
+function TS_plot_2d(featureData,TimeSeries,featureLabels,groupNames,annotateParams,showDistr,whatClassifier)
 % TS_plot_2d   Plots a dataset in a two-dimensional space.
 %
 % e.g., The space of two chosen features, or two principal components.
 %
 %---INPUTS:
-% Features, an Nx2 vector of where to plot each of the N data objects in the
+% featureData, an Nx2 vector of where to plot each of the N data objects in the
 %           two-dimensional space
 %
 % TimeSeries, structure array for time series metadata
@@ -50,7 +50,8 @@ function TS_plot_2d(Features,TimeSeries,featureLabels,groupNames,annotateParams,
 %% Check Inputs:
 % ------------------------------------------------------------------------------
 
-% Features should be a Nx2 vector of where to plot each of the N data objects in the two-dimensional space
+% featureData should be a Nx2 vector of where to plot each of the N data objects in
+% the two-dimensional space:
 if nargin < 1
     error('You must provide two-dimensional feature vectors for the data.')
 end
@@ -60,19 +61,19 @@ if nargin < 3 || isempty(featureLabels)
 end
 
 if nargin < 5 || isempty(annotateParams)
-    annotateParams = struct('n',0); % don't annotate
+    annotateParams = struct('n',6); % don't annotate
 end
 
 % By default, plot kernel density estimates above and on the side of the plot:
 if nargin < 6 || isempty(showDistr)
-    showDistr = 1;
+    showDistr = true;
 end
 
 if nargin < 7 || isempty(whatClassifier)
     whatClassifier = 'svm_linear';
 end
 
-makeFigure = 1; % default is to plot on a brand new figure('color','w')
+makeFigure = true; % default is to plot on a brand new figure('color','w')
 
 %-------------------------------------------------------------------------------
 % Preliminaries
@@ -118,7 +119,7 @@ if showDistr
     subplot(4,4,1:3); hold on; box('on')
     maxx = 0; minn = 100;
     for i = 1:numClasses
-        fr = BF_plot_ks(Features(groupLabels==i,1),groupColors{i},0);
+        fr = BF_plot_ks(featureData(groupLabels==i,1),groupColors{i},0);
         maxx = max([maxx,fr]); minn = min([minn,fr]);
     end
     axTop = gca;
@@ -130,7 +131,7 @@ if showDistr
     subplot(4,4,[8,12,16]); hold on; box('on')
     maxx = 0; minn = 100;
     for i = 1:numClasses
-        fr = BF_plot_ks(Features(groupLabels==i,2),groupColors{i},1);
+        fr = BF_plot_ks(featureData(groupLabels==i,2),groupColors{i},1);
         maxx = max([maxx,fr]); minn = min([minn,fr]);
     end
     axSide = gca;
@@ -155,7 +156,7 @@ else
 end
 
 for i = 1:numClasses
-    plot(Features(groupLabels==i,1),Features(groupLabels==i,2),...
+    plot(featureData(groupLabels==i,1),featureData(groupLabels==i,2),...
                 '.','color',groupColors{i},'MarkerSize',theMarkerSize)
 end
 
@@ -189,7 +190,7 @@ if isfield(TimeSeries,'Data')
     % Only attempt to annotate if time-series data is provided
 
     % Produce xy points
-    xy = Features;
+    xy = featureData;
 
     % Go-go-go:
     BF_AnnotatePoints(xy,TimeSeries,annotateParams);
@@ -204,9 +205,9 @@ if numClasses > 1
     classRate = zeros(3,1); % classRate1, classRate2, classRateboth
     try
         fprintf(1,'Estimating %u-class classification rates for each feature (and in combination)...\n',numClasses);
-        classRate(1) = GiveMeCfn(whatClassifier,Features(:,1),groupLabels,Features(:,1),groupLabels,numClasses);
-        classRate(2) = GiveMeCfn(whatClassifier,Features(:,2),groupLabels,Features(:,2),groupLabels,numClasses);
-        [classRate(3),~,whatLoss] = GiveMeCfn(whatClassifier,Features,groupLabels,Features(:,1:2),groupLabels,numClasses);
+        classRate(1) = GiveMeCfn(whatClassifier,featureData(:,1),groupLabels,featureData(:,1),groupLabels,numClasses);
+        classRate(2) = GiveMeCfn(whatClassifier,featureData(:,2),groupLabels,featureData(:,2),groupLabels,numClasses);
+        [classRate(3),~,whatLoss] = GiveMeCfn(whatClassifier,featureData,groupLabels,featureData(:,1:2),groupLabels,numClasses);
         % Record that classification was performed successfully:
         didClassify = 1;
         fprintf(1,'%s in 2-d space: %.2f%%\n',whatLoss,classRate(3));
@@ -220,12 +221,12 @@ if numClasses > 1
         fprintf(1,'Estimating classification boundaries...\n');
         try
             % Train the model (in-sample):
-            [~,Mdl] = GiveMeCfn(whatClassifier,Features,groupLabels,Features,groupLabels,numClasses);
+            [~,Mdl] = GiveMeCfn(whatClassifier,featureData,groupLabels,featureData,groupLabels,numClasses);
 
             % Predict scores over the 150x150 grid through space
             gridInc = 150;
-            [x1Grid,x2Grid] = meshgrid(linspace(min(Features(:,1)),max(Features(:,1)),gridInc),...
-                                       linspace(min(Features(:,2)),max(Features(:,2)),gridInc));
+            [x1Grid,x2Grid] = meshgrid(linspace(min(featureData(:,1)),max(featureData(:,1)),gridInc),...
+                                       linspace(min(featureData(:,2)),max(featureData(:,2)),gridInc));
             fullGrid = [x1Grid(:),x2Grid(:)];
             predLabels = predict(Mdl,fullGrid);
 
