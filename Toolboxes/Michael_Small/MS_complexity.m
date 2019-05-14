13f1e74b1fae1fb2cb30064fce5f5438266c47b8
Ben Fulcher <ben.d.fulcher@gmail.com>
2019-05-14 22:16:10 +1000

    Sahand's push to fix bits of Michael Small's LZ complexity code

--- Toolboxes/Michael_Small/MS_complexity.m
+++ Toolboxes/Michael_Small/MS_complexity.m
@@ -1,9 +1,8 @@
-% cmp = MS_complexity(x,n);
-%
-% calculate the Lempel-Ziv complexity of the n-bit encoding of x. 
+function cmp = MS_complexity(x,n,binHow)
+% Calculate the Lempel-Ziv complexity of the n-bit encoding of x.
 %
 % cmp is the normalised complexity, that is the number of distinct
-% symbol sequences in x, divided by the expected number of distinct 
+% symbol sequences in x, divided by the expected number of distinct
 % symbols for a noise sequence.
 %
 % Algorithm is implemented in complexitybs.c
@@ -16,20 +15,24 @@
 % Series A, vol. 52. World Scientific, 2005. (ISBN 981-256-117-X) and the
 % references therein.
 
-function cmp = MS_complexity(x,n);
-
 if nargin < 2
     n = 2;
 end
+if nargin < 3
+    binHow = 'equiprobable';
+end
+%-------------------------------------------------------------------------------
 
 if length(n) > 1
-   for ni = 1:length(n)
-        cmp(ni) = MS_complexity(x,n(ni)); 
-   end
-else,
-    
-    if 1,
-        %do the binning, with equi-probably bins
+    % Can run with multiple values of the number of symbols, n:
+    for ni = 1:length(n)
+        cmp(ni) = MS_complexity(x,n(ni),binHow);
+    end
+else
+
+    % do the binning, with equiprobable bins
+    switch binHow
+    case 'equiprobable'
         x=x(:);
         nx=length(x);
         [xn,xi]=sort(x+eps*randn(size(x))); %introduce randomness for ties
@@ -37,24 +40,24 @@ else,
         y=1:nx;
         y=floor(y.*(n/(nx+1)));
         x(xi)=y;
-    else,
-        %do binning with equal width bins
+    case 'equiwidth'
+    % else,
+    %     %do binning with equal width bins
         x=x(:);
         nx=length(x);
         minx=min(x);
         maxx=max(x);
         stepx=(maxx-minx)/n;
         y=zeros(nx,1);
-        while minx<maxx,
+        while minx<maxx
             minx=minx+stepx;
             y=y+double(x<minx);
-        end;
+        end
         x=floor(y);
-    end;
-    
-    %compute complexity with complexitybs
-    cmp=MS_complexitybs(x);
+    end
 
-end;
+    %compute complexity with complexitybs
+    cmp = MS_complexitybs(x);
+end
 
 end
