9ae6152a2c4fb1a69e63abfddc50947694e33307
Ben Fulcher <ben.d.fulcher@gmail.com>
2020-06-07 22:01:08 +1000

    stubborn

--- PeripheryFunctions/TS_LoadFeatures.m
+++ PeripheryFunctions/TS_LoadFeatures.m
@@ -3,18 +3,20 @@ function TS_LoadFeatures(whatDataFile,TS_DataMatToLoad,TS_CalcTimeToLoad,TS_Qual
 load(whatDataFile);
 
 if isempty(INP_opsToLoad)
-    myOpIds = 1:size(TS_DataMat,2);
+    myOpIDs = 1:size(TS_DataMat,2);
 else
-    myOpIds = TS_GetIDs(INP_opsToLoad,whatDataFile,'ops','Name');
+    myOpIDs = TS_GetIDs(INP_opsToLoad,whatDataFile,'ops','Name');
 end
 if isempty(INP_tsToLoad)
-    myTSIds = 1:size(TS_DataMat,1);
+    myTSIDs = 1:size(TS_DataMat,1);
 else
-    myTSIds = TS_GetIDs(INP_tsToLoad,whatDataFile,'ts','Name');
+    myTSIDs = TS_GetIDs(INP_tsToLoad,whatDataFile,'ts','Name');
 end
 
-TS_DataMat(myTSIds,myOpIds) = TS_DataMatToLoad;
-TS_CalcTime(myTSIds,myOpIds) = TS_CalcTimeToLoad;
-TS_Quality(myTSIds,myOpIds) = TS_QualityToLoad;
+TS_DataMat(myTSIDs,myOpIDs) = TS_DataMatToLoad;
+TS_CalcTime(myTSIDs,myOpIDs) = TS_CalcTimeToLoad;
+TS_Quality(myTSIDs,myOpIDs) = TS_QualityToLoad;
 
 save(whatDataFile,'TS_DataMat','TS_CalcTime','TS_Quality','-append');
+
+end
